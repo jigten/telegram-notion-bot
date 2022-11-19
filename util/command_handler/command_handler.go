dc7d592a711996c75298942e62bdab65844f12b6
Ugyen Jigten <ujigten@gmail.com>
2022-11-19 21:28:55 +0600

    Update countdown time

--- util/command_handler/command_handler.go
+++ util/command_handler/command_handler.go
@@ -15,7 +15,7 @@ func HandleCommand(command string) (string, error) {
 	}
 
 	if command == constants.COUNTDOWN_COMMAND {
-		nextMeet := time.Date(2022, time.October, 27, 16, 0, 0, 0, time.Local)
+		nextMeet := time.Date(2022, time.November, 24, 14, 0, 0, 0, time.Local)
 		year, month, day, hour, min, sec, done := diff(time.Now(), nextMeet)
 
 		if done {
